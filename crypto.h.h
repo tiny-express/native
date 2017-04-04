1572aba0fa4dfa6af0e3222dff12582c92042d2b
Loi Nguyen <loi.nguyen@lazada.com>
2017-04-05 03:39:18 +0700

    Add crypto header

---
+++ crypto.h.h
@@ -0,0 +1,12 @@
+//
+// Created by User on 4/5/17.
+//
+
+#ifndef NATIVE_CRYPTO_H_H
+#define NATIVE_CRYPTO_H_H
+
+#include "crypto/md5.h"
+#include "crypto/sha1.h"
+#include "crypto/base64.h"
+
+#endif //NATIVE_CRYPTO_H_H
