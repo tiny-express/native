9e8c7d62948da83af775d85a054b37f55765fa21
Loi Nguyen <loinguyentrung@gmail.com>
2017-04-16 03:24:09 +0700

    Prepare structure for http and vendor

--- general/len.c
+++ general/len.c
@@ -1,8 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
-#include <math.h>
 #include "../string.h"
 
 // Length of pointer
---
+++ vendor/notification.c
@@ -0,0 +1,3 @@
+// TODO
+// Apple Push Notification
+// Google Push Notification
