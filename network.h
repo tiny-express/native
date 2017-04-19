1cfa4464fea2d01a8fe974e6984324d31e62becf
dthongvl <thong_pro113520@yahoo.com>
2017-04-20 04:50:57 +0700

    Implement http_request

--- network.h
+++ network.h
@@ -15,6 +15,6 @@ char *http_schema(char *url);
 char *http_hostname(char *url);
 int   http_port(char *url);
 char *http_query(char *url);
-char *http_request(char *url, char *data, char *headers);
+char *http_request(char *method, char* url, char **headers, char **body);
 
 #endif
