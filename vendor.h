e8dc4852d191a3a1372bb497ce658e24ec310ec6
Loi Nguyen <loinguyentrung@gmail.com>
2017-04-22 06:23:26 +0700

    Refactor vendor

--- vendor.h
+++ vendor.h
@@ -1,6 +1,9 @@
 #ifndef VENDOR_ELASTICSEARCH_H
 #define VENDOR_ELASTICSEARCH_H
 
-char *search(char *host, char *index, char *query);
+char *etcd_get(char* host, char *key);
+char *etcd_set(char* host, char *key, char* value);
+
+char *es_query(char *host, char *index, char *query);
 
 #endif
--- vendor/etcd_test.c
+++ vendor/etcd_test.c
@@ -1,1 +1,14 @@
-// TODO
+#include "../builtin.h"
+#include "../unit_test.h"
+#include "../vendor.h"
+
+#define ETCD_MASTER "http://etcd.foodtiny.net:80"
+
+TEST(Builtin_Vendor, ETCDSet) {
+    char *result = etcd_set(ETCD_MASTER, "/elassandra/development/seeds", "1223");
+    ASSERT_TRUE(length_pointer_char(result) > 0);
+
+    result = etcd_set(ETCD_MASTER, "/elassandra/development/seeds", "Hello World");
+    ASSERT_TRUE(length_pointer_char(result) > 0);
+}
+
