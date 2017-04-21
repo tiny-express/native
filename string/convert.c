1b134bdfa6916ed7fc946b30a8293d0e942f1cad
Loi Nguyen <loinguyentrung@gmail.com>
2017-04-22 05:24:32 +0700

    Rename macro

--- string/convert.c
+++ string/convert.c
@@ -2,7 +2,7 @@
 #include <memory.h>
 #include "../string.h"
 
-#define CONVERT_STR(TYPE, FORMAT); \
+#define STR_FROM(TYPE, FORMAT); \
 char* string_from_##TYPE(TYPE target) {\
 	char *convert;\
 	asprintf(&convert, FORMAT, target);\
@@ -17,11 +17,11 @@ TYPE string_to_##TYPE(char *target) {\
     return result;\
 }
 
-CONVERT_STR(short,  "%d\0");
-CONVERT_STR(int,    "%d\0");
-CONVERT_STR(long,   "%ld\0");
-CONVERT_STR(float,  "%g\0");
-CONVERT_STR(double, "%.16g\0");
+STR_FROM(short,  "%d\0");
+STR_FROM(int,    "%d\0");
+STR_FROM(long,   "%ld\0");
+STR_FROM(float,  "%g\0");
+STR_FROM(double, "%.16g\0");
 
 STR_TO(short,  "%hi\0");
 STR_TO(int,    "%d\0");
