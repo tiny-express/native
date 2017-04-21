eb6dd53462fc1c5aa720f5b55ee964ab09c264a7
dquangit <phamvanquangit@gmail.com>
2017-04-22 05:32:51 +0700

    set, get etcd

--- etcd/etcd_test.c
+++ etcd/etcd_test.c
@@ -6,8 +6,21 @@
 #include "../unit_test.h"
 #include "../etcd.h"
 
-TEST(Builtin_ETCD, GetAllKeys) {
-    char *all_key = etcd_get_all_keys();
-    ASSERT_STR("{\"action\":\"get\",\"node\":{\"dir\":true,\"nodes\":[{\"key\":\"/xxx\",\"value\":\"123\",\"modifiedIndex\":4,\"createdIndex\":4}]}}", all_key);
+TEST(Builtin_ETCD, ETCDGetAllNode) {
+    char *all_key = etcd_get_all_node(ETCD_LOCALHOST);
+    ASSERT_TRUE(length_pointer_char(all_key) > 1);
+}
+
+TEST(BUILTIN_ETCD, ETCDGet) {
+    char *node = etcd_get(ETCD_LOCALHOST, "test_node");
+    ASSERT_TRUE(length_pointer_char(node) > 0);
+}
+
+TEST(BUILTIN_ETCD, ETCDSet) {
+    char *result = etcd_set(ETCD_MASTER, "/test_node", "1223");
+    ASSERT_TRUE(length_pointer_char(result) > 0);
+
+    result = etcd_set(ETCD_MASTER, "/test_node", "Hello World");
+    ASSERT_TRUE(length_pointer_char(result) > 0);
 }
 
