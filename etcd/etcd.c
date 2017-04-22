9591ccc13acd44cd641810cb0dacb72a4efcece7
dquangit <phamvanquangit@gmail.com>
2017-04-22 17:04:48 +0700

    check null etcd

--- etcd/etcd.c
+++ etcd/etcd.c
@@ -15,6 +15,11 @@ char *get_etcd_url(char *host);
  * @return json
  */
 char* etcd_get_all_node(char *host) {
+
+    if (length_pointer_char(host) == 0) {
+        return "";
+    }
+
     char *headers[2] = {
             "\0"
     };
@@ -44,7 +49,13 @@ char *etcd_get(char* host, char *key) {
     char *body[2] = {
             "\0"
     };
-    char* url = get_etcd_url(host);
+    char *url = get_etcd_url(host);
+    if (string_char_in_string(key, '/')) {
+        url = string_concat(url, key);
+    } else {
+        url = string_concat(url, "/");
+        url = string_concat(url, key);
+    }
     char* response = http_request(GET_METHOD, url, headers, body);
     int response_body_begin_index = string_index(response, "{", 1);
     int response_length = length_pointer_char(response);
@@ -60,6 +71,13 @@ char *etcd_get(char* host, char *key) {
  * @return json
  */
 char *etcd_set(char *host, char *key, char *value) {
+
+    if (length_pointer_char(host) == 0 ||
+            length_pointer_char(key) == 0 ||
+            length_pointer_char(value) == 0) {
+        return "";
+    }
+
     char *url = get_etcd_url(host);
     if (string_char_in_string(key, '/')) {
         url = string_concat(url, key);
@@ -89,6 +107,7 @@ char *etcd_set(char *host, char *key, char *value) {
             response_length - 2);
     return result;
 }
+
 /**
  * get url of request
  * @param host (consist of port, example: http://localhost:80/etcd/)
