2aadb1cb8beb3b8f848586429b0239eaad42c2e1
dquangit <phamvanquangit@gmail.com>
2017-04-25 02:58:33 +0700

    test mutex lock thread

--- thread/loop_test.c
+++ thread/loop_test.c
@@ -30,22 +30,33 @@ TEST(Builtin_Thread, SetInterval) {
 
 TEST(Builtin_Thread, SetIntervalMutex) {
     int t = 0;
+    int count1 = 0;
+    int count2 = 0;
     void *add_for_interval_mutex() {
+        count1 ++;
+        pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+        pthread_mutex_lock(&mutex);
         t ++;
+        pthread_mutex_unlock(&mutex);
     }
 
     void *subtract_for_interval_mutex() {
+        count2 ++;
+        pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+        pthread_mutex_lock(&mutex);
         t --;
+        pthread_mutex_unlock(&mutex);
     }
-    pthread_t pthread2 = set_interval(add_for_interval_mutex, 200);
-    pthread_t pthread3 = set_interval(subtract_for_interval_mutex, 100);
+    pthread_t pthread2 = set_interval(add_for_interval_mutex, 0);
+    pthread_t pthread3 = set_interval(subtract_for_interval_mutex, 20);
     while(1) {
         sleep(1);
         stop_thread(pthread3);
         stop_thread(pthread2);
         break;
     }
-    ASSERT_TRUE(t > 0);
+    printf("t count1 - count2: %d - %d\n",t, count1 - count2);
+    ASSERT_TRUE(t != count1 - count2);
 }
 
 TEST(Builtin_Thread, SetTimeOut) {
@@ -54,15 +65,35 @@ TEST(Builtin_Thread, SetTimeOut) {
 
     void *add_for_timeout() {
         a ++;
-        fflush(stdout);
     }
 
-    pthread_t fucking_pthread = set_time_out(add_for_timeout, 200);
+    pthread_t pthread = set_time_out(add_for_timeout, 200);
     ASSERT_EQUAL(0, a);
     while (1) {
         sleep(1);
-//        stop_thread(fucking_pthread);
         break;
     }
     ASSERT_EQUAL(1, a);
+}
+
+TEST(Builtin_Thread, TimeOutStopInterval) {
+    int x = 0;
+    int b = 0;
+    void *add() {
+        x ++;
+    }
+
+    pthread_t interval_thread;
+
+    void *stop_interval() {
+        stop_thread(interval_thread);
+        b = x;
+    }
+    interval_thread = set_interval(add, 500);
+    pthread_t time_out_thread = set_time_out(stop_interval, 2000);
+    while (1) {
+        sleep(5);
+        break;
+    }
+    ASSERT_TRUE(b > 0);
 }
