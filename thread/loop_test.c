eecb22e27d3ce7421291b5fd2dc197fe80c8c0fe
dquangit <phamvanquangit@gmail.com>
2017-04-26 00:07:28 +0700

    test loop

--- thread/loop_test.c
+++ thread/loop_test.c
@@ -15,23 +15,27 @@ TEST(Builtin_Thread, SetInterval) {
         t ++;
     }
 
-    void *subtract() {
-        t --;
-    }
-
     pthread_t pthread1 = set_interval(add, 100);
     while(1) {
         sleep(1);
-        stop_thread(pthread1);
+        pthread_cancel(pthread1);
         break;
     }
     ASSERT_TRUE(t > 0);
+}
+
+
+TEST(Builtin_Thread, SetIntervalNullCallback) {
+
+    pthread_t pthread1 = set_interval(NULL, 10);
+    ASSERT_NULL(pthread1);
 }
 
 TEST(Builtin_Thread, SetIntervalMutex) {
     int t = 0;
     int count1 = 0;
     int count2 = 0;
+
     void *add_for_interval_mutex() {
         count1 ++;
         pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -42,20 +46,17 @@ TEST(Builtin_Thread, SetIntervalMutex) {
 
     void *subtract_for_interval_mutex() {
         count2 ++;
-        pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-        pthread_mutex_lock(&mutex);
         t --;
-        pthread_mutex_unlock(&mutex);
     }
+
     pthread_t pthread2 = set_interval(add_for_interval_mutex, 0);
-    pthread_t pthread3 = set_interval(subtract_for_interval_mutex, 20);
+    pthread_t pthread3 = set_interval(subtract_for_interval_mutex, 0);
     while(1) {
         sleep(1);
-        stop_thread(pthread3);
-        stop_thread(pthread2);
+        pthread_cancel(pthread3);
+        pthread_cancel(pthread2);
         break;
     }
-    printf("t count1 - count2: %d - %d\n",t, count1 - count2);
     ASSERT_TRUE(t != count1 - count2);
 }
 
@@ -63,11 +64,11 @@ TEST(Builtin_Thread, SetTimeOut) {
 
     int a = 0;
 
-    void *add_for_timeout() {
+    void *add() {
         a ++;
     }
 
-    pthread_t pthread = set_time_out(add_for_timeout, 200);
+    pthread_t pthread = set_time_out(add, 200);
     ASSERT_EQUAL(0, a);
     while (1) {
         sleep(1);
@@ -86,14 +87,20 @@ TEST(Builtin_Thread, TimeOutStopInterval) {
     pthread_t interval_thread;
 
     void *stop_interval() {
-        stop_thread(interval_thread);
+        pthread_cancel(interval_thread);
         b = x;
     }
-    interval_thread = set_interval(add, 500);
-    pthread_t time_out_thread = set_time_out(stop_interval, 2000);
+    interval_thread = set_interval(add, 20);
+    pthread_t time_out_thread = set_time_out(stop_interval, 500);
     while (1) {
-        sleep(5);
+        sleep(1);
         break;
     }
     ASSERT_TRUE(b > 0);
 }
+
+TEST(Builtin_Thread, SetTimeOutNullCallback) {
+    pthread_t pthread = set_time_out(NULL, 0);
+    ASSERT_NULL(pthread);
+}
+
