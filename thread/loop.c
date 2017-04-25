59683dffd277b3f09ae5cf8c4f241ea5a59298a2
dquangit <phamvanquangit@gmail.com>
2017-04-26 00:07:28 +0700

    test loop

--- thread/loop.c
+++ thread/loop.c
@@ -7,8 +7,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
 typedef struct thread_argument {
     void *callback;
     unsigned int miliseconds;
@@ -20,6 +18,9 @@ void *run(void *argument);
 
 
 pthread_t set_interval(void *callback, unsigned int miliseconds) {
+    if (callback == NULL) {
+        return NULL;
+    }
     pthread_t thread;
     thread_argument *argument = malloc(sizeof(thread_argument));
     argument->miliseconds = miliseconds;
@@ -33,6 +34,10 @@ pthread_t set_interval(void *callback, unsigned int miliseconds) {
 }
 
 pthread_t set_time_out(void *callback, unsigned int miliseconds) {
+
+    if (callback == NULL) {
+        return NULL;
+    }
     pthread_t thread;
     thread_argument *argument = malloc(sizeof(thread_argument));
     argument->miliseconds = miliseconds;
@@ -46,7 +51,6 @@ pthread_t set_time_out(void *callback, unsigned int miliseconds) {
 }
 
 void *loop(void *argument) {
-//    pthread_mutex_lock(&mutex);
     while(1) {
         ((void(*)())((thread_argument*)argument)->callback)();
         usleep(((thread_argument *)argument)->miliseconds*1000);
@@ -55,13 +59,5 @@ void *loop(void *argument) {
 
 void *run(void *argument) {
     usleep(((thread_argument *)argument)->miliseconds*1000);
-//    pthread_mutex_t run_mutex = PTHREAD_MUTEX_INITIALIZER;
-//    pthread_mutex_lock(&run_mutex);
     ((void(*)())((thread_argument*)argument)->callback)();
-//    pthread_mutex_unlock(&run_mutex);
-}
-
-void stop_thread(pthread_t pthread) {
-//    pthread_mutex_unlock(&mutex);
-    pthread_cancel(pthread);
 }
