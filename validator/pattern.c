6a8bdbc819cd05b3644591cbff92b864e0dc0763
Frangte <1560268@student.hcmus.edu.vn>
2017-05-01 01:46:31 +0700

    Test validator

--- validator/pattern.c
+++ validator/pattern.c
@@ -8,7 +8,7 @@
 int match_url(char *url) {
 
     if (url == NULL || length_pointer_char(url) == 0) {
-        return 0;
+        return FALSE;
     }
 
     regex_t exp;
@@ -16,56 +16,58 @@ int match_url(char *url) {
 
     if (convert != 0) {
         printf("CAN'T CONVERT\n");
-        return 0;
+        return FALSE;
     }
 
     if (regexec(&exp, url, 0, NULL, 0) == 0) {
 
-        return 1;
+        return TRUE;
     }
 
     regfree(&exp);
-    return 0;
+    return FALSE;
 }
 
 int match_email(char *email) {
     if (email == NULL || length_pointer_char(email) == 0) {
-        return 0;
+        return FALSE;
     }
+
     regex_t exp;
     int convert = regcomp(&exp, EMAIL_PATTERN, REG_EXTENDED);
 
     if (convert != 0) {
         printf("CAN'T CONVERT\n");
-        return 0;
+        return FALSE;
     }
 
     if (regexec(&exp, email, 0, NULL, 0) == 0) {
 
-        return 1;
+        return TRUE;
     }
 
     regfree(&exp);
-    return 0;
+    return FALSE;
 }
 
 int match_phone_number(char *phone_number) {
-    if (phone_number == NULL || length_pointer_char(phone_number) < 10 || length_pointer_char(phone_number) > 11) {
-        return 0;
+    if (phone_number == NULL || length_pointer_char(phone_number) < 10 || length_pointer_char(phone_number) > 12) {
+        return FALSE;
     }
+
     regex_t exp;
     int convert = regcomp(&exp, PHONE_PATTERN, REG_EXTENDED);
 
     if (convert != 0) {
         printf("CAN'T CONVERT\n");
-        return 0;
+        return FALSE;
     }
 
     if (regexec(&exp, phone_number, 0, NULL, 0) == 0) {
 
-        return 1;
+        return TRUE;
     }
 
     regfree(&exp);
-    return 0;
+    return FALSE;
 }
