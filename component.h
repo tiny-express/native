69e5527e6ecbf548891035d974e5e049a0c619ab
Tu Cao <neacao@gmail.com>
2017-05-04 18:37:58 +0700

    remove component.h file

--- component.h
+++
@@ -1,9 +0,0 @@
-
-#define HANDLER_START "Start handler"
-#define HANDLER_END "End handler"
-
-#define REPOSITORY_START "Start repository"
-#define REPOSITORY_END "End repository"
-
-#define QUERY_START "Start query"
-#define QUERY_END "End query"
