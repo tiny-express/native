ffcd697dbae18ab9c24f70f61d7928670a0ded34
Loi Nguyen <loinguyentrung@gmail.com>
2017-05-15 22:50:25 +0700

    Check type casting for String

--- generic/string_test.cpp
+++ generic/string_test.cpp
@@ -1,24 +1,25 @@
-//extern "C" {
-//#include "../unit_test.h"
-//}
-//
-//#include "../native.h"
-//
-//TEST(Generic, String) {
-//    char *result1 = String(3);
-//    const char *expect1 = "3";
-//    ASSERT_STR(expect1, result1);
-//
-//    char *result2 = String(4039249.5023);
-//    const char *expect2 = "4039249.5023";
-//    ASSERT_STR(expect2, result2);
-//
-//    std::string str("foodtiny");
-//    char *result3 = String(str);
-//    const char *expect3 = "foodtiny";
-//    ASSERT_STR(expect3, result3);
-//    delete result3;
-//
+extern "C" {
+#include "../unit_test.h"
+}
+
+#include "../native.h"
+
+TEST(Generic, String) {
+
+    char *result1 = String(3);
+    const char *expect1 = "3";
+    ASSERT_STR(expect1, result1);
+
+    char *result2 = String(4039249.5023);
+    const char *expect2 = "4039249.5023";
+    ASSERT_STR(expect2, result2);
+
+    std::string str("foodtiny");
+    char *result3 = String(str);
+    const char *expect3 = "foodtiny";
+    ASSERT_STR(expect3, result3);
+    delete result3;
+
 //    std::vector<int> vectorInt(4, 2000);
 //    char *result4 = String(vectorInt);
 //    const char *expect4 = "[2000, 2000, 2000, 2000]";
@@ -30,5 +31,5 @@
 //    const char *expect5 = "[12.3457, 12.3457, 12.3457, 12.3457]";
 //    ASSERT_STR(expect5, result5);
 //    delete result5;
-//}
-//
+}
+
