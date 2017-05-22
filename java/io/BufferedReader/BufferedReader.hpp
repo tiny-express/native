f6ca4cacdecd7768a4082fdf5ad175a86678b3d5
Loi Nguyen <loinguyentrung@gmail.com>
2017-05-23 03:08:56 +0700

    Add class from Oracle documentation

--- java/io/BufferedReader/BufferedReader.hpp
+++ java/io/BufferedReader/BufferedReader.hpp
@@ -34,7 +34,7 @@ using namespace Java::Lang;
 
 namespace Java {
     namespace IO {
-        class BufferedReader : public virtual Reader {
+        class BufferedWriter : public virtual Reader {
 
         };
     }
