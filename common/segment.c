27d32e5a87ef01abdc8ccad79302b91634bcf823
nakhoa <nakhoa17@gmail.com>
2017-05-25 13:14:52 +0700

    Fix function segment_pointer_char in process.c

--- common/segment.c
+++ common/segment.c
@@ -34,16 +34,17 @@ inline TYPE *segment_pointer_##TYPE(TYPE *target, int from, int to) {\
 	if (target == NULL){\
 		return "";\
 	}\
-	int len_target = length_pointer_##TYPE(target);\
-	if (from > to || from < 0 || from > len_target || to < 0) {\
+	int length_target = length_pointer_##TYPE(target);\
+	if (from > to || from < 0 || from > length_target || to < 0) {\
 		return "";\
 	}\
-	int len = to - from + 1;\
-	if (to >= len_target) {\
-		len = len_target - from + 1;\
+	int length= to - from + 2;\
+	if (to >= length_target) {\
+		length= length_target - from + 1;\
 	}\
-	TYPE *pointer = calloc(len, sizeof(TYPE));\
-	memcpy(pointer, &target[from], len);\
+	TYPE *pointer = calloc(length, sizeof(TYPE));\
+	memcpy(pointer, &target[from], length- 1);\
+	pointer[length] = '\0';\
 	return pointer;\
 }
 
@@ -52,23 +53,24 @@ inline TYPE *segment_pointer_constant_##TYPE(const TYPE *target, int from, int t
 	if (target == NULL){\
 		return "";\
 	}\
-	int len_target = length_pointer_##TYPE((TYPE*) target);\
-	if (from > to || from < 0 || from > len_target || to < 0) {\
+	int length_target = length_pointer_##TYPE((TYPE*) target);\
+	if (from > to || from < 0 || from > length_target || to < 0) {\
 		return "";\
 	}\
-	int len = to - from + 1;\
-	if (to >= len_target) {\
-		len = len_target - from + 1;\
+	int length= to - from + 2;\
+	if (to >= length_target) {\
+		length= length_target - from + 1;\
 	}\
-	TYPE *pointer = calloc(len, sizeof(TYPE));\
-	memcpy(pointer, &target[from], len);\
+	TYPE *pointer = calloc(length, sizeof(TYPE));\
+	memcpy(pointer, &target[from], length- 1);\
+    pointer[length] = '\0';\
 	return pointer;\
 }
 
 #define P_P_SEG(TYPE);\
 inline TYPE **segment_pointer_pointer_##TYPE(TYPE **target, int from, int to) {\
-    int len_target = length_pointer_pointer_##TYPE(target);\
-	if (from > to || from < 0 || from > len_target || to < 0 || to > len_target) {\
+    int length_target = length_pointer_pointer_##TYPE(target);\
+	if (from > to || from < 0 || from > length_target || to < 0 || to > length_target) {\
 		return NULL;\
 	}\
 	TYPE **pointer = calloc((to - from + 2), sizeof(TYPE*));\
