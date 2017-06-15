a3a58bf1e16be0fe8f9023c9f3a6d06291d62257
Loi Nguyen <loinguyentrung@gmail.com>
2017-06-15 21:49:00 +0700

    Add hello world program

---
+++ example/HelloWorld/Person.hpp
@@ -0,0 +1,11 @@
+#ifndef NATIVE_EXAMPLE_PERSON_HPP
+#define NATIVE_EXAMPLE_PERSON_HPP
+
+class Person {
+public:
+	Person() {
+	
+	}
+};
+
+#endif //NATIVE_EXAMPLE_PERSON_HPP
---
+++ example/HelloWorld/Teacher.hpp
@@ -0,0 +1,14 @@
+//
+// Created by loint on 15/06/2017.
+//
+
+#ifndef NATIVE_TEACHER_HPP
+#define NATIVE_TEACHER_HPP
+
+
+class Teacher {
+
+};
+
+
+#endif //NATIVE_TEACHER_HPP
