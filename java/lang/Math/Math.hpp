f8b962040f13ce4c3a29e708fbf70215bcd66ee5
TuanAnhNguyen69 <razordmonkey@live.com>
2017-07-07 13:38:57 +0700

    #138 Declare all method

--- java/lang/Math/Math.hpp
+++ java/lang/Math/Math.hpp
@@ -34,26 +34,71 @@
 namespace Java {
 	namespace Lang {
 		class Math final : public virtual Object {
+		private:
+			//static Random randomNumberGenerator;
+			static long negativeZeroDoubleBits;
+			static long negativeZeroFloatBits;
+		private:
+			Math(){}
+			static void initRGN();
 		public:
-			static double abs(double a);
-			static float abs(float a);
+			static constexpr double E = 2.7182818284590452354;
+			static constexpr double PI = 3.14159265358979323846;
+		public:
 			static int abs(int a);
+			static float abs(float a);
 			static long abs(long a);
+			static double abs(double a);
+			static double acos(double a);
+			static double asin(double a);
+			static double atan(double a);
+			static double atan2(double a, double b);
+			static double cbrt(double a);
+			static double ceil(double a);
+			static double copySign(double a, double b);
+			static float copySign(float a, float b);
+			static double cos(double a);
+			static double cosh(double a);
+			static double exp(double a);
+			static double expm1(double a);
 			static double floor(double a);
-			static double max(double a, double b);
-			static float max(float a, float b);
+			static int getExponent(double a);
+			static int getExponent(float a);
+			static double hypot(double a, double b);
+			static double IEEEremainder(double a, double b);
+			static double log(double a);
+			static double log10(double a);
+			static double log1p(double a);
 			static int max(int a, int b);
+			static float max(float a, float b);
 			static long max(long a, long b);
-			static double min(double a, double b);
-			static float min(float a, float b);
+			static double max(double a, double b);
 			static int min(int a, int b);
+			static float min(float a, float b);
+			static long min(long a, long b);
+			static double min(double a, double b);
+			static double nextAfter(double a, double b);
+			static float nextAfter(float a, double b);
+			static float nextUp(float a);
+			static double nextUp(double a);
 			static double pow(double a, double b);
 			static double random();
-			static long round(double a);
+			static double rint(double a);
 			static int round(float a);
+			static long round(double a);
+			static float scalb(float a, int b);
+			static double scalb(double a, int b);
+			static double signum(double a);
+			static float signum(float a);
+			static double sin(double a);
+			static double sinh(double a);
 			static double sqrt(double a);
-			
-			string toString() const;
+			static double tan(double a);
+			static double tanh(double a);
+			static double toDegrees(double a);
+			static double toRadians(double a);
+			static float ulp(float a);
+			static double ulp(double a);
 		};
 	}
 }
