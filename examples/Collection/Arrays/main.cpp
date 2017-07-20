071437ea407ac3bc6f5080f5b8ffe89bfcbcc517
TuanAnhNguyen69 <razordmonkey@live.com>
2017-07-20 20:36:19 +0700

    #76 fix Arrays

--- examples/Collection/Arrays/main.cpp
+++ examples/Collection/Arrays/main.cpp
@@ -2,20 +2,20 @@
 
 void ArraysSort()
 {
-	// ERROR - Must support properly for two ways
 	long arrayLongNormal[5] = { 3 , -2 , 4 , 0 , -1 };
 	Arrays::sort(arrayLongNormal, 0, 4);
-//	Array<long> arrayLongArray = { 3 , -2 , 4 , 0 , -1 };
-//	Arrays::sort(arrayLongArray, 0, 4);
+
+	Array<long> arrayLongArray = { 3 , -2 , 4 , 0 , -1 };
+    Arrays::sort(&arrayLongArray, 0, 4);
 }
 
 void ArraysSearch()
 {
-	// ERROR - Must support properly for two ways
 	long arrayLongNormal[5] = { 3 , -2 , 4 , 0 , -1 };
 	Arrays::binarySearch(arrayLongNormal, 4, 5);
-//	Array<long> arrayLongArray = { 3 , -2 , 4 , 0 , -1 };
-//	Arrays::binarySearch(arrayLongArray, 4, 5);
+
+	Array<long> arrayLongArray = { 3 , -2 , 4 , 0 , -1 };
+	Arrays::binarySearch(arrayLongArray, 4, 5);
 }
 
 int main()
