f098f08996b159b4adec48348da1cadcd1bf586f
Truong Chau Hien <truongchauhien@gmail.com>
2017-07-21 10:17:51 +0700

    #132 add test for constructor

---
+++ java/util/PriorityQueue/PriorityQueueTest.cpp
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2017 Food Tiny Project. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+extern "C" {
+#include "../../../kernel/test.h"
+}
+
+#include "PriorityQueue.hpp"
+
+using namespace Java::Util;
+
+TEST(JavaUtil, PriorityQueueConstructor) {
+    PriorityQueue<int> defaultConstructor;
+    ASSERT_EQUAL(0, defaultConstructor.size());
+
+    PriorityQueue<int> initializerListConstructor {1, 2, 3, 4, 5};
+    ASSERT_EQUAL(5, initializerListConstructor.size());
+    ASSERT_EQUAL(5, initializerListConstructor.peek());
+
+    PriorityQueue<int> copyConstructor(initializerListConstructor);
+    ASSERT_EQUAL(5, copyConstructor.size());
+    ASSERT_EQUAL(5, copyConstructor.peek());
+}
+
+TEST(JavaUtil, PriorityQueueDestructor) {
+    PriorityQueue<int> priorityQueue1;
+
+    PriorityQueue<int> *priorityQueue2 = new PriorityQueue<int>();
+    delete priorityQueue2;
+}
+
