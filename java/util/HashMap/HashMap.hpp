bf7a38c434d647a366cc131bffb4a12ba4596d88
Truong Hoang Minh <truonghoangminh1302@gmail.com>
2017-09-17 10:30:40 +0700

    #50 Added more declaration

--- java/util/HashMap/HashMap.hpp
+++ java/util/HashMap/HashMap.hpp
@@ -33,7 +33,6 @@
 #include "../AbstractMap/AbstractMap.hpp"
 #include "../Map/Map.hpp"
 #include "../Set/Set.hpp"
-#include "../../lang/NullPointerException/NullPointerException.hpp"
 
 namespace Java {
 	namespace Util {
@@ -88,6 +87,215 @@ namespace Java {
 			}
 		public:
 			/**
+			 * Compares the specified object
+			 * with this map for equality.
+			 *
+			 * @param object
+			 */
+//			boolean equals(Object object) {
+//				if (o == this)
+//					return true;
+//				if (o instanceof Map.Entry) {
+//					Map.Entry<?, ?> e = (Map.Entry<?, ?>) o;
+//					if (Objects.equals(key, e.getKey())
+//						&& Objects.equals(value, e.getValue()))
+//						return true;
+//				}
+//				return false;
+//			}
+
+			// TODO(thoangminh): Need class BiFunction to implement this method
+			/**
+			 * forEach method
+			 */
+//			void forEach(BiConsumer<? super K, ? super V> action) {
+//				Node<K, V>[] tab;
+//				if (action == null)
+//					throw new NullPointerException();
+//				if (size > 0 && (tab = table) != null) {
+//					int mc = modCount;
+//					for (int i = 0; i < tab.length; ++i) {
+//						for (Node<K, V> e = tab[i]; e != null; e = e.next)
+//							action.accept(e.key, e.value);
+//					}
+//					if (modCount != mc)
+//						throw new ConcurrentModificationException();
+//				}
+//			}
+
+			/**
+			 * Returns the hash code value for this map.  The hash code of a map is
+			 * defined to be the sum of the hash codes of each entry in the map's
+			 * entrySet view.  This ensures that m1.equals(m2)
+			 * implies that m1.hashCode() == m2.hashCode() for any two maps
+			 * m1 and m2, as required by the general contract of
+			 * Object.
+			 *
+			 * This implementation iterates over entrySet(), calling
+			 * {Map.Entry hashCode()} on each element (entry) in the
+			 * set, and adding up the results.
+			 *
+			 * @return 	the hash code value for this map
+			 */
+//			int hashCode() {
+//				int h = 0;
+//				Iterator<Entry<K, V>> i = entrySet().iterator();
+//				while (i.hasNext())
+//					h += i.next().hashCode();
+//				return h;
+//			}
+
+			// TODO(thoangminh): Support this method later
+			/**
+			 * Returns a Set view of the keys contained in this map.
+			 * The set is backed by the map, so changes to the map are
+			 * reflected in the set, and vice-versa.
+			 *
+			 * If the map is modified
+			 * while an iteration over the set is in progress (except through
+			 * the iterator's own remove operation), the results of
+			 * the iteration are undefined.
+			 *
+			 * The set supports element removal,
+			 * which removes the corresponding mapping from the map, via the
+			 * Iterator.remove, Set.remove,
+			 * removeAll, retainAll, and clear
+			 * operations.
+			 *
+			 * It does not support the add or addAll
+			 * operations.
+			 *
+			 * @return a set view of the keys contained in this map
+			 */
+//			Set<K> keySet() {
+//				Set<K> ks;
+//				return (ks = keySet) == null ? (keySet = new KeySet()) : ks;
+//			}
+
+//			final class KeySet extends AbstractSet<K> {
+//				public final int size() {
+//					return size;
+//				}
+
+			/**
+			 * If the specified key is not already associated with a value or is associated with null,
+			 * associates iteratorToString with the given non-null value.
+			 *
+			 * @param 	key 		key with which the resulting value is to be associated
+			 *
+			 * @param 	value 		the non-null value to be merged with the
+			 * 						existing value associated with the key or,
+			 * 						if no existing value or a null value
+			 * 						is associated with the key, to be associated with the key
+			 *
+			 * @param 	BiFunction 	remappingFunction - the function to recompute
+			 * 						a value if present
+			 * @return 	Value 		the new value associated with the specified key,
+			 * 						or null if no value is associated with the key
+			 */
+//			V merge(K key, V value,
+//					   BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
+//				if (value == null)
+//					throw new NullPointerException();
+//				if (remappingFunction == null)
+//					throw new NullPointerException();
+//				int hash = hash(key);
+//				Node<K, V>[] tab;
+//				Node<K, V> first;
+//				int n, i;
+//				int binCount = 0;
+//				TreeNode<K, V> t = null;
+//				Node<K, V> old = null;
+//				if (size > threshold || (tab = table) == null
+//					|| (n = tab.length) == 0)
+//					n = (tab = resize()).length;
+//				if ((first = tab[i = (n - 1) & hash]) != null) {
+//					if (first instanceof TreeNode)
+//					old = (t = (TreeNode<K, V>) first).getTreeNode(hash, key);
+//					else {
+//						Node<K, V> e = first;
+//						K k;
+//						do {
+//							if (e.hash == hash
+//								&& ((k = e.key) == key || (key != null && key
+//									.equals(k)))) {
+//								old = e;
+//								break;
+//							}
+//							++binCount;
+//						} while ((e = e.next) != null);
+//					}
+//				}
+//				if (old != null) {
+//					V v;
+//					if (old.value != null)
+//						v = remappingFunction.apply(old.value, value);
+//					else
+//						v = value;
+//					if (v != null) {
+//						old.value = v;
+//						afterNodeAccess(old);
+//					} else
+//						removeNode(hash, key, null, false, true);
+//					return v;
+//				}
+//				if (value != null) {
+//					if (t != null)
+//						t.putTreeVal(this, tab, hash, key, value);
+//					else {
+//						tab[i] = newNode(hash, key, value, first);
+//						if (binCount >= TREEIFY_THRESHOLD - 1)
+//							treeifyBin(tab, hash);
+//					}
+//					++modCount;
+//					++size;
+//					afterNodeInsertion(true);
+//				}
+//				return value;
+//			}
+
+			/**
+			 * Reset to initial default state.
+			 * Called by clone and readObject.
+			 */
+//			void reinitialize() {
+//				table = null;
+//				entrySet = null;
+//				keySet = null;
+//				values = null;
+//				modCount = 0;
+//				threshold = 0;
+//				size = 0;
+//			}
+
+			/**
+			 * Returns a {Collection} view of the values contained in this map.
+			 * The collection is backed by the map, so changes to the map are
+			 * reflected in the collection, and vice-versa.  If the map is
+			 *
+			 * modified while an iteration over the collection is in progress
+			 * (except through the iterator's own remove operation),
+			 * the results of the iteration are undefined.  The collection
+			 * 
+			 * supports element removal, which removes the corresponding
+			 * mapping from the map, via the Iterator.remove,
+			 * Collection.remove, removeAll,
+			 * retainAll and clear operations.  It does not
+			 * support the add or addAll operations.
+			 *
+			 * @return a view of the values contained in this map
+			 */
+//			Collection<V> values() {
+//				Collection<V> vs;
+//				return (vs = values) == null ? (values = new Values()) : vs;
+//			}
+//
+//			final class Values extends AbstractCollection<V> {
+//				public final int size() {
+//					return size;
+//				}
+
+			/**
 			 * Removes all of the mappings from this map.
 			 * The map will be empty after this call returns.
 			 */
@@ -115,8 +323,10 @@ namespace Java {
 			 * its current mapped value (or null if there is no current mapping).
 			 *
 			 * @param 	key 		key with which the specified value is to be associated
+			 *
 			 * @param 	BiFunction 	remappingFunction - the function to compute a value
-			 * @return 	Value 			the new value associated with the specified key,
+			 *
+			 * @return 	Value 		the new value associated with the specified key,
 			 * 						or null if none
 			 */
 //			Value compute(Key key, BiFunction<? super Key,? super Value,? extends Value> remappingFunction);
@@ -129,7 +339,9 @@ namespace Java {
 			 * the given mapping function and enters iteratorToString into this map unless null.
 			 *
 			 * @param 	key 		key with which the specified value is to be associated
+			 *
 			 * @param 	Function 	mappingFunction - the function to compute a value
+			 *
 			 * @return 	Value 		the current (existing or computed) value
 			 * 						associated with the specified key,
 			 * 						or null if the computed value is null
@@ -141,8 +353,10 @@ namespace Java {
 			 * If the value for the specified key is present and non-null,
 			 * attempts to compute a new mapping given the key and its current mapped value.
 			 *
-			 * @param 	Key key  	key with which the specified value is to be associated
+			 * @param 	key  		key with which the specified value is to be associated
+			 *
 			 * @param 	BiFunction 	remappingFunction - the function to compute a value
+			 *
 			 * @return 	Value  		the new value associated with the specified key,
 			 * 						or null if none
 			 */
@@ -151,7 +365,8 @@ namespace Java {
 			/**
 			 * Returns true if this map contains a mapping for the specified key.
 			 *
-			 * @param 	Key key 		The key whose presence in this map is to be tested
+			 * @param 	key 		The key whose presence in this map is to be tested
+			 *
 			 * @return 	true  		if this map contains a mapping for the specified key.
 			 * 			false 		otherwise
 			 */
@@ -162,7 +377,8 @@ namespace Java {
 			/**
 			 * Returns true if this map maps one or more keys to the specified value.
 			 *
-			 * @param 	Value value 	value whose presence in this map is to be tested
+			 * @param 	value 		value whose presence in this map is to be tested
+			 *
 			 * @return 	true  		if this map maps one or more keys to the specified value
 			 *  		false 		otherwise
 			 */
@@ -202,9 +418,10 @@ namespace Java {
 			 * Returns the value to which the specified key is mapped,
 			 * or null if this map contains no mapping for the key
 			 *
-			 * @param 	Key key 		the key whose associated value is to be returned
+			 * @param 	key 			the key whose associated value is to be returned
+			 *
 			 * @return 	Value value 	the value to which the specified key is mapped,
-			 * 						or null if no mapping with key
+			 * 							or null if no mapping with key
 			 */
 			Value get(const Key &key) const {
 				Value result;
@@ -223,7 +440,9 @@ namespace Java {
 			 *  or defaultValue if this map contains no mapping for the key.
 			 *
 			 * @param 	key 			the key whose associated value is to be returned
+			 *
 			 * @param 	defaultValue 	the default mapping of the key
+			 *
 			 * @return 	Value 			the value to which the specified key is mapped,
 			 * 							or defaultValue if this map contains no mapping
 			 * 							for the key
@@ -244,37 +463,12 @@ namespace Java {
 			/**
 			 * Returns true if this map contains no key-value mappings.
 			 *
-			 * @return 	true : 	true if this map contains no key-value mappings
-			 *  		false: 	otherwise
+			 * @return 	true  	true if this map contains no key-value mappings
+			 *  		false 	otherwise
 			 */
 			boolean isEmpty() const {
 				return this->original.size() == 0;
 			}
-
-            // TODO(thoangminh): Support this method later
-			/**
-			 * Returns a Set view of the keys contained in this map
-			 * @return Set<Key> - a set view of the keys contained in this map
-			 */
-//			Set<Key> keySet();
-
-			/**
-			 * Don't support this method
-			 *
-			 * If the specified key is not already associated with a value or is associated with null,
-			 * associates iteratorToString with the given non-null value.
-			 *
-			 * @param 	Key key 		key with which the resulting value is to be associated
-			 * @param 	value 		the non-null value to be merged with the
-			 * 						existing value associated with the key or,
-			 * 						if no existing value or a null value
-			 * 						is associated with the key, to be associated with the key
-			 * @param 	BiFunction 	remappingFunction - the function to recompute
-			 * 						a value if present
-			 * @return 	Value 			the new value associated with the specified key,
-			 * 						or null if no value is associated with the key
-			 */
-//			Value merge(Key key, Value value, BiFunction<? super Value,? super Value,? extends Value> remappingFunction);
 
 			/**
 			 * Returns a shallow copy of this HashMap instance:
@@ -305,7 +499,9 @@ namespace Java {
 			 * associates iteratorToString with the given value and returns null, else returns the current value.
 			 *
 			 * @param 	key 	key with which the specified value is to be associated
+			 *
 			 * @param 	value 	value to be associated with the specified key
+			 *
 			 * @return 	Value	the previous value associated with the specified key,
 			 * 					or null if there was no mapping for the key.
 			 * 					(A null return can also indicate
@@ -329,8 +525,9 @@ namespace Java {
 			/**
 			 * Removes the mapping for the specified key from this map if present.
 			 *
-			 * @param 	Key key 	key whose mapping is to be removed from the map
-			 * @return 	Value 		the previous value associated with key,
+			 * @param 	key 	key whose mapping is to be removed from the map
+			 *
+			 * @return 	Value 	the previous value associated with key,
 			 * 					or null if there was no mapping for key.
 			 * 					(A null return can also indicate that the map
 			 * 					previously associated null with key.)
@@ -352,11 +549,12 @@ namespace Java {
 			/**
 			 * Removes the entry for the specified key only if iteratorToString is currently mapped to the specified value.
 			 *
-			 * @param 	Key key 		key with which the specified value is associated
-			 * @param 	Value value 	value expected to be associated with the specified key
-			 * @return 	boolean
-			 * 			true : 		if the value was removed
-			 * 			false: 		if otherwise
+			 * @param 	key 		key with which the specified value is associated
+			 *
+			 * @param 	value 		value expected to be associated with the specified key
+			 *
+			 * @return 	true  		if the value was removed
+			 * 			false 		if otherwise
 			 */
 			boolean remove(const Key &key, const Value &value) {
 				auto const iteratorToString = this->original.find(key);
@@ -375,8 +573,10 @@ namespace Java {
 			 * Replaces the entry for the specified key only if iteratorToString is currently mapped to some value.
 			 *
 			 * @param 	key 	key with which the specified value is associated
+			 *
 			 * @param 	value 	value to be associated with the specified key
-			 * @return 	Value 		the previous value associated with the specified key,
+			 *
+			 * @return 	Value 	the previous value associated with the specified key,
 			 * 					or null if there was no mapping for the key.
 			 * 					(A null return can also indicate that the map
 			 * 					previously associated null with the key,
@@ -400,11 +600,13 @@ namespace Java {
 			 * Replaces the entry for the specified key only if currently mapped to the specified value.
 			 *
 			 * @param 	key 		key with which the specified value is associated
+			 *
 			 * @param 	oldValue 	value expected to be associated with the specified key
+			 *
 			 * @param 	newValue 	value to be associated with the specified key
-			 * @return 	boolean
-			 * 			true : 		if the value was replaced
-			 * 			false: 		if otherwise
+			 *
+			 * @return 	true  		if the value was replaced
+			 * 			false 		if otherwise
 			 */
 			boolean replace(const Key &key,
 							const Value &oldValue,
@@ -508,7 +710,8 @@ namespace Java {
 		private:
 			// TODO(anhnt): Need String support unicode for unicode character
 			/**
-             * Replace escape sequence by raw string of that sequence to using in Json
+             * Replace escape sequence by raw string of
+             * that sequence to using in Json
              *
              * @param stringToReplace
              * @return a String with add escape sequence replaced
