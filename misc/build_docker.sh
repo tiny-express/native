e9fbd4339db576126c2b07c3d269fdeffbf15270
dthongvl <thong_pro113520@yahoo.com>
2017-09-19 09:45:19 +0700

    Update build docker in master

--- misc/build_docker.sh
+++ misc/build_docker.sh
@@ -4,6 +4,13 @@ rm -rf CMakeCache.txt
 if [[ "$TRAVIS_BRANCH" == "development" ]];
 then
     docker login -u $UP -p $DOWN
-    docker build -t foodtiny/native:development -f DockerfileDevelopment .
+    docker build -t foodtiny/native:development .
     docker push foodtiny/native:development
+fi
+
+if [[ "$TRAVIS_BRANCH" == "master" ]];
+then
+    docker login -u $UP -p $DOWN
+    docker build -t foodtiny/native:latest .
+    docker push foodtiny/native:latest
 fi
