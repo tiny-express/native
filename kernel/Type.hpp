556bb430bd21b767faea15613c4858ac549c5cc8
dthongvl <thong_pro113520@yahoo.com>
2017-10-05 21:51:31 +0700

    Add platform check for boolean

--- kernel/Type.hpp
+++ kernel/Type.hpp
@@ -33,7 +33,11 @@
 #include <cstddef>
 #include <cwchar>
 
+#ifdef WIN
+#define boolean bool;
+#else
 typedef bool boolean;
+#endif
 typedef char *string;
 typedef const char *const_string;
 typedef wchar_t unicode;
