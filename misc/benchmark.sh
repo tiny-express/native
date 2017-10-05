611f08a98fd4ac3f354f2b2cb9a3b04c9501e234
Loi Nguyen <loinguyentrung@gmail.com>
2017-10-06 00:55:48 +0700

    Test travis build

--- misc/benchmark.sh
+++ misc/benchmark.sh
@@ -3,4 +3,4 @@ cd /tmp
 git clone https://github.com/google/benchmark.git
 cd benchmark
 cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_LTO=true
-sudo make install
+make benchmark && sudo make install
