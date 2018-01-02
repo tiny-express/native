ca9a48aa43db79b042d04d13298cd3b4825a79c0
Loi Nguyen <loinguyentrung@gmail.com>
2018-01-02 02:44:13 +0000

    Ignore checking google testing

--- misc/benchmark.sh
+++ misc/benchmark.sh
@@ -2,5 +2,5 @@
 cd /tmp
 git clone https://github.com/google/benchmark.git
 cd benchmark
-cmake -DCMAKE_BUILD_TYPE=Release
+cmake -DBENCHMARK_ENABLE_TESTING=OFF -DCMAKE_BUILD_TYPE=Release .
 make benchmark && sudo make install
