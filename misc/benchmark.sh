c4e8b50f926752960879b2ffe9ce5bba319c1033
Nguyen Trung Loi <loint@users.noreply.github.com>
2018-01-02 10:03:18 +0700

    Update benchmark.sh

--- misc/benchmark.sh
+++ misc/benchmark.sh
@@ -3,4 +3,4 @@ cd /tmp
 git clone https://github.com/google/benchmark.git
 cd benchmark
 cmake -DBENCHMARK_ENABLE_TESTING=OFF -DCMAKE_BUILD_TYPE=Release .
-make benchmark && sudo make install
+make benchmark -j8 && sudo make install
