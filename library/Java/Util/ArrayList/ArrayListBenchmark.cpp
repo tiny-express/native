40f211df1011747f50c63e0aeb66b7be4df1b01c
Loi Nguyen <loinguyentrung@gmail.com>
2018-04-14 01:40:02 +0700

    Refactor benchmark - put test library inside library

--- library/Java/Util/ArrayList/ArrayListBenchmark.cpp
+++ library/Java/Util/ArrayList/ArrayListBenchmark.cpp
@@ -24,7 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "../../../../benchmark/benchmark.hpp"
+#include "../../../../misc/benchmark/benchmark.hpp"
 
 #include "ArrayList.hpp"
 
